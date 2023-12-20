c985c24962426a77cead42641d886067c6450038
maresyp <mattq.2000@icloud.com>
2023-12-20 20:59:35 +0100

    Add documentation

--- app/src/main/java/com/example/weatherapp/api/WeatherDataProvider.kt
+++ app/src/main/java/com/example/weatherapp/api/WeatherDataProvider.kt
@@ -9,6 +9,12 @@ import java.net.HttpURLConnection
 import java.net.URL
 
 class WeatherDataProvider {
+    /**
+     * Downloads weather data from the OpenWeatherMap API.
+     *
+     * @param cityName The name of the city to download weather data for.
+     * @return A JSONObject containing the weather data, or null if an error occurred.
+     */
     suspend fun downloadWeatherData(cityName: String): JSONObject? = withContext(Dispatchers.IO) {
         val apiKey: String = BuildConfig.OpenWeatherMapApiKey
         val url = URL("https://api.openweathermap.org/data/2.5/forecast?q=$cityName&appid=$apiKey")
