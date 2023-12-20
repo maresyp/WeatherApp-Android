c0c5d983f3c2dc2d12983912e439c3c7e98d9652
maresyp <mattq.2000@icloud.com>
2023-12-20 11:32:52 +0100

    Add WeatherDataManager and search functionality to MainFragment

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
 
@@ -13,5 +14,22 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val searchView = view.findViewById<SearchView>(R.id.search_view)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // Handle search query submission
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                // Handle search query text change
+                return false
+            }
+        })
     }
 }
