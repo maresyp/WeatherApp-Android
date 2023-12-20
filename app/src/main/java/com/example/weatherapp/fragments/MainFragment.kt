b88b124f736e4e6f7452d9a0a0f40c58603ae8a6
maresyp <mattq.2000@icloud.com>
2023-12-20 17:47:45 +0100

    Update import statement for SearchView

--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
 
