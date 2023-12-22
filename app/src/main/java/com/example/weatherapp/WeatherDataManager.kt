0324f1a7f435d6444b0ebaf5cee979b44135a835
maresyp <mattq.2000@icloud.com>
2023-12-22 21:54:11 +0100

    Add network availability check and logging

--- app/src/main/java/com/example/weatherapp/WeatherDataManager.kt
+++ app/src/main/java/com/example/weatherapp/WeatherDataManager.kt
@@ -1,10 +1,13 @@
 package com.example.weatherapp
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.util.Log
 import com.example.weatherapp.api.WeatherDataProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
+class NetworkNotAvailableException(message: String) : Exception(message)
 
 class WeatherDataManager {
 
@@ -20,8 +23,24 @@ class WeatherDataManager {
      */
     private suspend fun downloadAndSaveData(filename: String, query: String, context: Context): JSONObject? {
         val data = weatherDataProvider.downloadWeatherData(query)
-        data?.let { saveData(filename, it, context) } // null safe operation (if data is not null)
+        data?.let {
+            saveData(filename, it, context)
+            Log.d("WeatherDataManager", "Saved data to $filename")
+        } // null safe operation (if data is not null)
+
         return data
+    }
+
+    /**
+     * Checks if the device is connected to the internet.
+     *
+     * @param context The context to use to check the connection.
+     * @return true if the device is connected to the internet, false otherwise.
+     */
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
     }
 
     /**
@@ -31,23 +50,30 @@ class WeatherDataManager {
      * @param context The context to use to get the data.
      * @return A JSONObject containing the weather data, or null if an error occurred.
      */
-    suspend fun getData(query: String, context: Context): JSONObject? = withContext(Dispatchers.IO) {
-        val filename = "$query.json"
-        if (context.fileList().contains(filename)) {
+    suspend fun getData(query: String, context: Context, forceDownload: Boolean): JSONObject? = withContext(Dispatchers.IO) {
+        if (!isNetworkAvailable(context)) {
+            throw NetworkNotAvailableException("No network connection")
+        }
+
+        val filename = "${query.lowercase()}.json"
+        if (context.fileList().contains(filename) && !forceDownload) {
+            Log.d("WeatherDataManager", "Reading data from $filename")
             readData(filename, context)?.let { data -> // null safe operation (if data is not null)
 
                 /* get timestamp from saved data and current time */
                 val timestamp = data.getJSONArray("list").getJSONObject(0).getLong("dt")
-                val currentTime = System.currentTimeMillis() / 1000L
+                val currentTime = System.currentTimeMillis() / 1000L // convert to seconds
 
                 /* if the data is older than 1 hour, download new data */
                 if (currentTime - timestamp > 3600) {
+                    Log.d("WeatherDataManager", "Data is older than 1 hour, downloading new data for $query")
                     downloadAndSaveData(filename, query, context)
                 } else {
                     data
                 }
             }
         } else {
+            Log.d("WeatherDataManager", "Downloading data for $query")
             downloadAndSaveData(filename, query, context)
         }
     }
@@ -69,7 +95,7 @@ class WeatherDataManager {
             /* return the number of bytes written */
             byteArray.size
         } catch (e: Exception) {
-            println("Error: ${e.message}")
+            Log.e("WeatherDataManager", "Error saving data to $filename", e)
             0
         }
     }
@@ -87,7 +113,7 @@ class WeatherDataManager {
                 JSONObject(it.readText())
             }
         } catch (e: Exception) {
-            println("Error: ${e.message}")
+            Log.e("WeatherDataManager", "Error reading data from $filename", e)
             null
         }
     }
