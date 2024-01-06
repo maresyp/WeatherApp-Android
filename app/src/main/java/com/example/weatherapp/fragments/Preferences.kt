c569f8d1e848a87d294d15c4477b9b8ba1fb19aa
maresyp <mattq.2000@icloud.com>
2024-01-06 11:36:27 +0100

    Add unit conversion and update weather data on unit change

--- app/src/main/java/com/example/weatherapp/fragments/Preferences.kt
+++ app/src/main/java/com/example/weatherapp/fragments/Preferences.kt
@@ -2,36 +2,59 @@ package com.example.weatherapp.fragments
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
+import com.example.weatherapp.NetworkNotAvailableException
 import com.example.weatherapp.R
 import com.example.weatherapp.WeatherDataManager
 import kotlinx.coroutines.launch
 
 // https://developer.android.com/develop/ui/views/components/settings?hl=en
 class Preferences : PreferenceFragmentCompat() {
+
+    private val weatherDataManager = WeatherDataManager()
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
         findPreference<SwitchPreferenceCompat>("units")
             ?.setOnPreferenceChangeListener { _, newValue ->
-                Log.d("Preferences", "Notifications enabled: $newValue")
-                // TODO : handle change of units
+                val unit = if (newValue as Boolean) {
+                    "metric"
+                } else {
+                    "imperial"
+                }
+
+                Log.d("Preferences", "Changing units: $unit")
+                weatherDataManager.setUnits(unit, requireContext())
+                weatherDataManager.getCurrentLocationFromPreferences(requireContext())?.let {
+                    val query = it.getJSONObject("city").getString("name")
+                    lifecycleScope.launch {
+                        try {
+                            weatherDataManager.getData(query, requireContext(), true)
+                        } catch (e: NetworkNotAvailableException) {
+                            Log.e("Preferences", "No network connection", e)
+                            Toast.makeText(requireContext(), "No network connection", Toast.LENGTH_SHORT).show()
+                        } catch (e: Exception) {
+                            Log.e("Preferences", "Unable to update weather data", e)
+                            Toast.makeText(requireContext(), "Unable to update weather data", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
                 true // Return true if the event is handled.
             }
 
         findPreference<Preference>("clearFiles")
             ?.setOnPreferenceClickListener {
                 Log.d("Preferences", "clearFiles was clicked")
-                val weatherDataManager = WeatherDataManager()
                 lifecycleScope.launch {
                     weatherDataManager.clearLocalData(requireContext())
                     weatherDataManager.clearCurrentLocationFromPreferences(requireContext())
                 }
                 true // Return true if the click is handled.
             }
-
     }
 }
